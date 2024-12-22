e5bcfde0323e2992434062b039892717cbbd9d5b
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2024-12-22 22:24:59 +0530

    variables

--- helloworld.sh
+++ helloworld.sh
@@ -2,5 +2,15 @@
 
 echo "helloworld , Iam learning devops "
 
+person1=suresh
+person2=ramesh
+
+
+
+echo " $person1 : Hi how are you $person2 "  
+echo " $person2 : Hi how are you  $person1"  
+echo " $person1 : Good Morning $sperson2"
+echo " $person1 : Good Morning   $person1"
+
 
 
