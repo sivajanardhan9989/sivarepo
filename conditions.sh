975a220ac9b6e0a6a3e6181ffc713190ec0ce88e
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2024-12-23 12:50:32 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# check if the number is greater than 10 or not
+Number=$1
+
+if [ $NUMBER -gt 10 ]
+then 
+    echo " $Number  is greater than 10 "
+else 
+      echo "$NUMBER is not greater than 10"
+fi 
---
+++ installgit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+userid=$(id -u)
+
+
+if [ $userid -ne 0 ]
+then 
+    echo "error : please run script with root user "
+    exit 1
+fi 
+
+yum install mysql -y 
