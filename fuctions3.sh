f335306ea8a8c2c2542cd0bafeb1f4fb83f03685
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2024-12-24 20:40:49 +0530

    fun 3

--- fuctions3.sh
+++ fuctions3.sh
@@ -3,7 +3,7 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-if [$user -ne 0]
+if [$user -ne 0 ] 
 then 
     echo  -e "error :: please run scripit with root access $N"
     exit 1 
@@ -14,3 +14,4 @@ for i in $@
 do 
     yum install $i -y 
 done 
+
