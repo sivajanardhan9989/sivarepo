8bb9c90e6f236d26f6220e89345b251af7bbda7e
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2024-12-25 00:54:00 +0530

    change

--- deletelogs.sh
+++ deletelogs.sh
@@ -8,7 +8,7 @@ LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 
-FILES_TO_DELETE=$(find  $APP_LOGS_DIR -name "*.log" -type  f -mtime +14EC)
+FILES_TO_DELETE=$(find  $APP_LOGS_DIR -name "*.log" -type  f -mtime +14)
 
  echo "$FILES_TO_DELETE"
 
