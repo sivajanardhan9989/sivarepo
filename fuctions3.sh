f2141903b789e9111b62226b4bc9ef6789eb9e6d
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2024-12-24 19:49:05 +0530

    functions2.sh

---
+++ fuctions3.sh
@@ -0,0 +1,16 @@
+userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+if [$user -ne 0]
+then 
+    echo  -e "error :: please run scripit with root access $N"
+    exit 1 
+
+fi 
+
+for i in $@
+do 
+    yum install $i -y 
+done 
